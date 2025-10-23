45e71227858abc978619e1b7fd540a76e15b292c
amShubhama <shubham14sk32@gmail.com>
2025-10-23 20:22:34 +0530

    feat(airplane): add association between airplane and flight models

--- src/models/flight.js
+++ src/models/flight.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.Airplane, {
+        foreignKey: 'airplaneId',
+        as: 'airplaneDetail'
+      });
     }
   }
   Flight.init({
