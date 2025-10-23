9622dd5d00319339b57c5c740258ecb3c38b67a6
amShubhama <shubham14sk32@gmail.com>
2025-10-23 20:14:29 +0530

    fix: delete files

--- src/utils/helper.js
+++
@@ -1,9 +0,0 @@
-const compareTime = (timeString1, timeString2) => {
-    let time1 = new Date(timeString1).getTime();
-    let time2 = new Date(timeString2).getTime();
-    return time1 > time2;
-}
-
-module.exports = {
-    compareTime,
-}
